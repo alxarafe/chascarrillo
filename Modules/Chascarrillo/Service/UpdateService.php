1759ae65c54eadcb8ae0059355677ba14a7cd058
Rafael San José <rsanjose@alxarafe.com>
2026-02-19 23:06:06 +0100

    style: enhance blockquotes and page content layout (v0.6.16)

--- Modules/Chascarrillo/Service/UpdateService.php
+++ Modules/Chascarrillo/Service/UpdateService.php
@@ -7,7 +7,7 @@ use Alxarafe\Lib\Messages;
 
 class UpdateService
 {
-    public const VERSION = 'v0.6.15';
+    public const VERSION = 'v0.6.16';
     public const UPDATE_URL = 'https://api.github.com/repos/alxarafe/chascarrillo/releases/latest';
 
     /**

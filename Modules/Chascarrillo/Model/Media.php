5ef126594c3ff22932c722b27eb1ccad0f7cbcc3
Rafael San José <rsanjose@alxarafe.com>
2026-02-19 07:59:42 +0100

    fix: add @property annotations to Media model to satisfy PHPStan

--- Modules/Chascarrillo/Model/Media.php
+++ Modules/Chascarrillo/Model/Media.php
@@ -10,6 +10,20 @@ namespace Modules\Chascarrillo\Model;
 
 use Alxarafe\Base\Model\Model;
 
+/**
+ * Class Media
+ * 
+ * @property int $id
+ * @property string $filename
+ * @property string $type
+ * @property string|null $mime_type
+ * @property string $path
+ * @property int $size
+ * @property string|null $alt_text
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class Media extends Model
 {
     protected $table = 'media';
